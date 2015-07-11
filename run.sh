d303f178783023bcc6e19ce9e90da327da5fa264
maeqiu <maeqiu@users.noreply.github.com>
2015-07-10 22:49:47 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 #output directory: tweet_output
 
 echo "Starting..."
-echo "Running the 1st feature, output to tweet_input/ft1.txt"
+echo "Running the 1st feature, output to tweet_output/ft1.txt"
 python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
 
-echo "Running the 2nd feature, output to tweet_input/ft2.txt"
+echo "Running the 2nd feature, output to tweet_output/ft2.txt"
 python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
 
 echo "Done!"
